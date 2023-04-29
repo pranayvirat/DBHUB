2a90cad7aa2d859b63dbb12a9ca7a837b8cf8b4b
yakshwanth <yakshwanth@gmail.com>
2023-04-29 01:53:14 -0400

    Intialised function and API calls

--- Client/frontend/src/components/mongodb.js
+++ Client/frontend/src/components/mongodb.js
@@ -4,41 +4,232 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import mongo from '.././logos/mongodb.png'
+import { Form } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button';
+import {Table,Alert} from 'react-bootstrap';
+import axios from 'axios';
+import { useState } from "react";
+import Modal from 'react-bootstrap/Modal';
 
 
-const MongoDB = () => {
-    return(
-        <>
-        <Navbar />
-        <div style={{ display: 'block', 
-                  width: 700, 
-                  padding: 30 }}>
-                    
-      <h4>MongoDB</h4>
-      <Card style={{ width: '40rem' }}>
-      <Card.Img variant="top" src={mongo} alt="mongo" style={{width:250, height:250}} />
-      <Card.Body>
-        <Card.Title>ABOUT</Card.Title>
-        <Card.Text>
-        MongoDB is a document-oriented NoSQL database system that is designed to be scalable and flexible. MongoDB uses a flexible document data model that can accommodate a wide variety of data types and structures.   
-        <br />
-        Some key features of AWS include:
-        <br />
-        <ul>
-            <li>Scalability: MongoDB is designed to be highly scalable, allowing businesses to easily scale out their database clusters as needed. This makes it a good choice for applications that need to handle high volumes of data and traffic. </li>
-            <li>Performance: MongoDB is designed for high performance, with features such as automatic sharding, in-memory storage engine, and indexing. These features allow businesses to run their applications faster and more efficiently.</li>
-            <li>Security: MongoDB offers a range of security features, including encryption, access controls, and auditing. These features can help businesses protect their data from unauthorized access and attacks.</li>
-        </ul>
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item><a href="https://www.mysql.com/" >Learn More</a></ListGroup.Item>
-      </ListGroup>
-    </Card>
-    </div>
-        </>
-      
-    )
+
+
+
+function RenderModal(mongoData) {
+  const [show, setShow] = useState(true);
+  const {data} = mongoData;
+ 
+  return (
+    <>
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        dialogClassName="modal-90w"
+        aria-labelledby="example-custom-modal-styling-title"
+        style={{
+          width: "2000px",
+          height: "100%",
+          marginTop: "0px",
+          marginRight: "0px",
+          marginLeft: "0px",
+          marginBottom: "0px",
+          overflow: "auto",
+          // position: "fixed",
+        
+
+        }}
+        size = 'xl'
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Postgres DATA
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+ <Table striped bordered hover style={{
+          marginTop: "30px",
+          marginRight: "0px",
+          width: "100%",
+        }}>
+          <thead>
+            <tr>
+              {data && Object.keys(data[0]).map((key, index) => (
+                <th key={index}>{key}</th>
+              ))}
+            </tr>
+          </thead>
+
+          <tbody style={{
+
+          }}>
+            
+            {data && data.map((row, index) => (
+              <tr key={index}>
+                {Object.keys(row).map((key, index) => (
+                <td key={index}>{row[key]} </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </Table> 
+
+
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 }
 
-export default MongoDB;
+export default function MongoDB() {
+
+
+
+  const [temp,setTemp] = useState('');
+  const [tables, setTables] = useState([]);
+  const [tableName, setTableName] = useState("");
+  const [formData, setFormData] = useState({
+    uri: '',
+    database: '',
+    collection: '',
+    fileType: ''
+  });
+  const [checked, setChecked] = useState(false);
+  const [connectionOutput, setConnectionOutput] = useState('');
+  const [mongoData, setmongoData] = useState([]);
+  const [viewData, setViewData] = useState(false);
+  const [type, setType] = useState('noFile');
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('URL value:', formData.url);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log('URL value:', formData.uri);
+    console.log('Database value:', formData.database);
+    axios.defaults.baseURL = "http://localhost:3000"
+    axios
+      .get("/api/mongo/mongoConnection", {
+        params: {
+          uri: formData.uri,
+          database: formData.database,
+        },
+      })
+      .then(async (result) => {
+        setConnectionOutput(result.data.output);
+        console.log(result.data);
+        //  window.alert(connectionOutput)
+       
+      });
+    
+  };
+  const [showAlert, setShowAlert] = useState(true);
+
+  const handleCloseAlert = () => setShowAlert(false);
+
+
+  const handleTables = (event) => {
+    event.preventDefault();
+    axios.defaults.baseURL = "http://localhost:3000"
+    axios.get("/api/mongo/mongoCollection", {
+      params: {
+        uri: formData.uri,
+        database: formData.database,
+      },
+    })
+      .then((result) => {
+        //  console.log(result.data.output);
+        setTables(result.data.output);
+        setTemp(result.data.output);
+        console.log(temp)
+        //console.log(tables);
+      });
+  };
+  const GetData = (event) => {
+
+    event.preventDefault();
+  
+      axios.defaults.baseURL = "http://localhost:3000"
+      axios.get("/api/mongo/mongoData", {
+        params: {
+          uri: formData.uri,
+          database: formData.database,
+          collection: formData.collection,
+          type: type
+        },
+      })
+        .then((result) => {
+
+          const parsedData = JSON.parse(result.data.output);
+          console.log(parsedData)
+          setmongoData(parsedData);
+          setViewData(true);
+          //console.log(result.data);
+        
+
+        });
+    
+  };
+  const handleClick1 = async (event) => {
+    try {
+      event.preventDefault();
+      
+      const response = await axios.get('http://localhost:3000/api/db/downloadMongo',{
+       params:{
+         fileType: type,
+       },
+       responseType: 'blob',
+      });
+     
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'data.zip');
+      document.body.appendChild(link);
+      link.click();
+      console.log(type)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  return (
+    <>
+      <Navbar />
+      
+    </>
+
+  )
+}
