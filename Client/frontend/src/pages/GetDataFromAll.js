50e66b9a332f901589452a577871f097481e5d0f
manideep9949 <mthotakura@albany.edu>
2023-04-29 11:05:32 +0530

    initial version of centerlized data

--- Client/frontend/src/pages/GetDataFromAll.js
+++ Client/frontend/src/pages/GetDataFromAll.js
@@ -7,59 +7,6 @@ import Button from 'react-bootstrap/Button';
 
 
 
-function RenderTable(fullData){
-  const {data} = fullData;
-
-  return(
-<div style={{
-        display: "grid",
-        textAlign: "center",
-         width: "90%",
-        alignItems:"center",
-        margin: "0 auto",
-        padding: "20px",
-        bottom: "50%",
-      }}>
-
-        <h4 style={{
-          textAlign: "center",
-          fontWeight: "bold",
-          letterSpacing: "1px",
-        }}>
-        Centralized data</h4>
-        <Table striped bordered hover style={{
-          marginTop:"30px",
-        }}>
-         
-           <thead>
-            <tr>
-              {data && Object.keys(data[0]).map((key, index) => (
-                <th key={index}>{key}</th>
-              ))}
-            </tr>
-          </thead>
-
-          
-          <tbody style={{
-            textAlign: "center",
-            
-
-}}>
-  
-  {data && data.map((row, index) => (
-    <tr key={index}>
-      {Object.keys(row).map((key, index) => (
-      <td key={index}>{row[key]} </td>
-      ))}
-    </tr>
-  ))}
-</tbody>
-        </Table>
-        
-      </div> 
-
-  )
-}
 
 const GetData = () => {
   const [tableData, setTableData] = useState([]);
@@ -144,7 +91,122 @@ else if(checkedCSV){
   return (
     <div>
 
+      <Navbar />
       
+      <div style={{
+      display: 'flex',
+      // width: 700,
+      // padding: 30
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#FEFEFA',
+    }}>
+      <div style={{
+        display: 'block',
+        margin: 30,
+      }}>
+      <Form onSubmit={handleSubmit} style={{
+        border: "1px solid grey",
+        padding: "20px",
+        borderRadius: "8px",
+        width: "50rem",
+        top: "50%",
+        left: "50%",
+        
+
+      }}>
+        <Form.Group>
+          <Form.Label style={{
+            fontWeight: "bold",
+            letterSpacing: "1px",
+          }}>Enter table name:</Form.Label>
+          <Form.Control type="text" name="url" onChange={handleChange}
+            placeholder="Enter Table name " required />
+          <br />
+        </Form.Group>
+       
+       <div style={{
+          display: "flex",
+          padding: "10px",
+          flexDirection: 'row',
+      alignItems: 'left',
+      justifyContent: 'left',
+          
+       }}>
+         <div style={{
+            display: "flex",
+            padding: "10px",
+            flexDirection: 'row',
+      alignItems: 'left',
+      justifyContent: 'left',
+      
+         }}>
+        <Form.Check type="checkbox" label="JSON" checked={checkedJSON} onChange={() => setCheckedJSON(!checkedJSON)} />
+        </div>
+        <div style={{
+            display: "flex",
+            padding: "10px",
+            flexDirection: 'row',
+      alignItems: 'left',
+      justifyContent: 'left',
+      
+         }}>
+        <Form.Check type="checkbox" label="Parquet" checked={checkedParquet} onChange={() => setCheckedParquet(!checkedParquet)} />
+        </div >
+        <div style={{
+            display: "flex",
+            padding: "10px",
+            flexDirection: 'row',
+      alignItems: 'left',
+      justifyContent: 'left',
+      
+         }}>
+        <Form.Check type="checkbox" label="CSV" checked={checkedCSV} onChange={() => setCheckedCSV(!checkedCSV)} />
+        </div>
+        </div>
+        <div style={{
+          display: "flex",
+          justifyContent: "end",
+          padding: "10px"
+        }}>
+          <Button variant="primary" type="submit"  style={{
+            marginRight: "10px",
+            backgroundColor: "#00D100",
+            border: "none",
+            color: "white"
+
+          }}>
+            Get Data
+          </Button>
+         <button style={{
+            backgroundColor: "#00D100",
+            border: "none",
+            color: "white",
+            borderRadius: "8px",
+            padding: "10px",
+            cursor: "pointer",
+            fontSize: "16px",
+            boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",
+            
+
+            
+          }}onClick={handleClick}>
+            Download
+            </button>
+            
+            
+          
+        </div>
+      </Form>
+      </div>
+      </div>
+      <div>
+            { showTable && <RenderTable data={tableData} />}
+            </div>
+
+
+       
     </div>
   );
 };
