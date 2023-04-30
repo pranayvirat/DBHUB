8124d12b1e12457f11b6c7e22e8f987c7f697e29
PrathikshaKola <pkola@albany.edu>
2023-04-30 19:51:37 -0400

    updated app.js

--- Server/app.js
+++ Server/app.js
@@ -59,9 +59,6 @@ app.get('/execute-spark-job', (req, res) => {
 });
 
 
-
-
-
 app.get('/execute-spark-retrieve-job', (req, res) => {
 
   const {url,username,password} = req.query;
@@ -111,6 +108,70 @@ app.get('/execute-spark-job-mysql', (req, res) => {
   });
 });
 
+// All Spark api for table retrieval
+app.get('/execute-spark-job-all', (req, res) => {
+  // Define command and arguments
+  const command = 'spark-submit';
+  const connections = {
+    "postgres":{
+      "url":"",
+      "username":"",
+      "password":""
+    },
+    "mysql":{
+      "url":"",
+      "username":"",
+      "password":""
+    }
+  }
+ const {table,file} = req.query;
+console.log(file)
+  //const input1 = '{"postgres":{"url":"jdbc:postgresql://54.236.43.43:5432/postgres","username": "postgres","password":"password"},"mysql":{"url":"jdbc:mysql://54.236.43.43:3307/MySQL","username":"root","password":"root"}}';
+  const args = ['--class', 'com.jdbc.getAll','--packages','org.mongodb.spark:mongo-spark-connector_2.12:10.1.1', '--driver-class-path', './jars/mysql-connector-j-8.0.32.jar:./jars/postgresql-42.3.7.jar', '--master', 'local[*]', './jars/getall_2.12-0.1.0-SNAPSHOT.jar',table,file];
+
+  // Spawn child process to execute command
+  const sparkJob = spawn(command, args);
+
+  // Store output from child process
+  let output = '';
+
+  // Handle stdout data from child process
+  sparkJob.stdout.on('data', (data) => {
+    const message = data.toString();
+    if(!message.includes("loading settings")){
+
+    
+    console.log(`stdout: ${data}`);
+    output += message;
+  }
+  });
+
+  sparkJob.stderr.on('data', (data) => {
+    console.error(`stderr: ${data}`);
+  });
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  // Handle error data from child process
+  sparkJob.stderr.on('data', (data) => {
+    //console.error(`stderr: ${data}`);
+  });
+
+  // When child process exits, check if there were any errors
+  sparkJob.on('exit', (code) => {
+    if (code !== 0) {
+      res.status(500).json({
+        message: `Spark job failed with exit code ${code}`,
+        output: output
+      });
+    } else {
+      res.json({
+        message: 'Spark job completed successfully',
+        output: output
+      });
+    }
+  });
+});
 
 //Mysql Spark api for table retrieval
 app.get('/execute-spark-retrieve-job-mysql', (req, res) => {
@@ -138,7 +199,6 @@ app.get('/execute-spark-retrieve-job-mysql', (req, res) => {
   });
 });
 
-
 /*
 app.get('/execute-spark-retrieve-job', (req, res) => {
   const url = 'jdbc:postgresql://localhost:5432/postgres';
@@ -161,68 +221,6 @@ app.get('/execute-spark-retrieve-job', (req, res) => {
   });
 });
 */
-// All Spark api for table retrieval
-app.get('/execute-spark-job-all', (req, res) => {
-// Define command and arguments
-  const command = 'spark-submit';
-  const connections = {
-    "postgres":{
-      "url":"",
-      "username":"",
-      "password":""
-    },
-    "mysql":{
-      "url":"",
-      "username":"",
-      "password":""
-    }
-  }
- const {table,file} = req.query;
- console.log(file)
-  //const input1 = '{"postgres":{"url":"jdbc:postgresql://54.236.43.43:5432/postgres","username": "postgres","password":"password"},"mysql":{"url":"jdbc:mysql://54.236.43.43:3307/MySQL","username":"root","password":"root"}}';
-  const args = ['--class', 'com.jdbc.getAll','--packages','org.mongodb.spark:mongo-spark-connector_2.12:10.1.1', '--driver-class-path', './jars/mysql-connector-j-8.0.32.jar:./jars/postgresql-42.3.7.jar', '--master', 'local[*]', './jars/getall_2.12-0.1.0-SNAPSHOT.jar',table,file];
-
-  // Spawn child process to execute command
-  const sparkJob = spawn(command, args);
-
-  // Store output from child process
-  let output = '';
-
-  // Handle stdout data from child process
-  sparkJob.stdout.on('data', (data) => {
-    const message = data.toString();
-    if(!message.includes("loading settings")){
-
-    
-    console.log(`stdout: ${data}`);
-    output += message;
-  }
-  });
-  sparkJob.stderr.on('data', (data) => {
-    console.error(`stderr: ${data}`);
-  });
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  res.setHeader('Pragma', 'no-cache');
-  res.setHeader('Expires', '0');
-  // Handle error data from child process
-  sparkJob.stderr.on('data', (data) => {
-    //console.error(`stderr: ${data}`);
-  });
- // When child process exits, check if there were any errors
-  sparkJob.on('exit', (code) => {
-    if (code !== 0) {
-      res.status(500).json({
-        message: `Spark job failed with exit code ${code}`,
-        output: output
-      });
-    } else {
-      res.json({
-        message: 'Spark job completed successfully',
-        output: output
-      });
-    }
-  });
-});
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
