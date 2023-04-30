a2b9cc9116f1d726baebfa55a825ae2342088263
Yakshwanth Mopidevi <60382827+yakshwanth@users.noreply.github.com>
2023-04-30 13:27:47 -0400

    Create mongoRoutes.js

---
+++ Server/routes/mongoRoutes.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var router = express.Router();
+const { spawn } = require('child_process');
+const child_process = require('child_process');
+
+module.exports = router;
