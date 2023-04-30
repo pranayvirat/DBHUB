129349acdbb8d5d2ae8f14e2516f7e7354302c3f
bhav2805 <125397084+bhav2805@users.noreply.github.com>
2023-04-30 14:27:48 -0400

    Added Background image in App.js

--- Client/frontend/src/App.js
+++ Client/frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import  Navbar from'./components/Navbar'
+import bg from './logos/bgImage.jpeg'
 function App() {
   return (
     <div>
@@ -10,7 +11,19 @@ function App() {
       <h1>What is DB HUB?</h1>
       A place where you can access multiple Database System hubs.
       </article>
-      </div>
+    </div>
+    <img src={bg} alt="bg" style={{
+            background:"rgba(0,0,0,0.5)",
+            // filter:"brightness(0.4)",
+            opacity:"0.3",
+            width: '100%',
+            height: '100%',
+            // maxHeight: '40000px',
+            // position: 'relative',
+            // backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+          }}/>
       </div>
   );
 }
