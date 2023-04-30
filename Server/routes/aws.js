a741ea60ceb8f57660f4d2d277ecffb890ef95a3
Ravi Teja <113475146+ravitvavilala@users.noreply.github.com>
2023-04-30 14:50:15 -0400

    defined constants and spark cmnd

--- Server/routes/aws.js
+++ Server/routes/aws.js
@@ -1,1 +1,20 @@
+var express = require("express");
+var router = express.Router();
+const { spawn } = require('child_process');
+const child_process = require('child_process');
 
+
+
+
+router.get('/awsRetrieval', (req, res) => {
+    const {accessKey,secretKey,bucketName,filePath,fileType} = req.query;
+    // Define command and arguments
+    const command = 'spark-submit';
+    const args = ['--class', 'com.jdbc.awsRetrieval', '--packages','org.apache.hadoop:hadoop-aws:3.3.1', '--master', 'local[*]', '/home/pranay/SE/sample_projects/jars/aws_2.12-0.1.0-SNAPSHOT.jar',accessKey,secretKey,bucketName,filePath,fileType];
+  
+    // Spawn child process to execute command
+    const sparkJob = spawn(command, args);
+  
+   // Store output from child process
+   let output = '';
+  });
